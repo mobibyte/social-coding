f2d73fc9310b54ebb9d123df9d03667d1055634c
Devrat <devratpatel1@gmail.com>
2024-02-15 17:43:30 -0600

    timer w/o styling

---
+++ apps/devrat/time-till-new-year/script.js
@@ -0,0 +1,20 @@
+var countDownDate = new Date("Jan 1, 2025 00:00:00").getTime();
+
+var countdownFunction = setInterval(function () {
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("timer").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(countdownFunction);
+    document.getElementById("timer").innerHTML = "Happy New Year!";
+  }
+}, 1000);
