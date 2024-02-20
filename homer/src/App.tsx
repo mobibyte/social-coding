8ff4c751276a5abf0dcdf186e6709907655b30b7
Diego V <diego@codewith.mobi>
2024-02-19 23:17:24 -0600

    feat: add mobi logo

--- homer/src/App.tsx
+++ homer/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import mobiLogo from './assets/mobi.png'
 import './App.css'
 
 function App() {
@@ -9,11 +8,8 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={mobiLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
