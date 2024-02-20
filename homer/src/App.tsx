f20d4dc5e19f1e872aba60f4b8e6730d76e75bf9
Diego V <diego@codewith.mobi>
2024-02-19 23:19:44 -0600

    feat: add mobi info to home page

--- homer/src/App.tsx
+++ homer/src/App.tsx
@@ -8,21 +8,18 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={mobiLogo} className="logo react" alt="React logo" />
+        <a href="https://codewith.mobi" target="_blank">
+          <img src={mobiLogo} className="logo mobi" alt="Mobi logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Mobi</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Discover the magic of making apps!
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Mobi logo to learn more
       </p>
     </>
   )
