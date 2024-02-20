5d7ea740f04431aab72b99bbae5cb43867bf10d2
Diego V <diego@codewith.mobi>
2024-02-19 23:23:27 -0600

    fix: remove unused code

--- homer/src/App.tsx
+++ homer/src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import mobiLogo from './assets/mobi.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
