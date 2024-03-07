6b73324e1f09cd73d73559288170ccd792079732
betim-hodza <bhodza02@gmail.com>
2024-03-06 20:59:24 -0600

    fixed a space

--- web/src/index.js
+++ web/src/index.js
@@ -209,8 +209,7 @@ let apps = [
   {
     title: "Dog App",
     link: "../apps/dog_app/mobi.html",
-    coverImg:
-      "https://images.unsplash.com/photo-1546238232-20216dec9f72?q=80&w=2368&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    coverImg:"https://images.unsplash.com/photo-1546238232-20216dec9f72?q=80&w=2368&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     author: "Devrat",
     authorImg: "../apps/devrat/kevin_minion.png",
   },
