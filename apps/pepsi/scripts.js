37cf64c18a7fdfa29facdd2cd01c4c6f94526a36
Pepsi <epicendless122@gmail.com>
2024-04-04 18:49:20 -0500

    updated files

--- apps/pepsi/scripts.js
+++ apps/pepsi/scripts.js
@@ -1,4 +1,14 @@
 function button_background()
 {
+    Color_Toggle = 'dark blue';
 
+    document.addEventListener("click")
+
+
+    if(document.onclick.getElementById == 'button1')
+    {
+        document.body.style.backgroundColor = Color_Toggle;
+    }
+
+    return document.getElementById
 }
